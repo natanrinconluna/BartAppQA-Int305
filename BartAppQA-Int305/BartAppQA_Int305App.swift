307581c04901ebdd3bd4b986535f4f671d2af9c6
Natan Rincon Luna <rincon.luna.natan@gmail.com>
2023-01-18 22:46:38 -0800

    Initial Commit

---
+++ BartAppQA-Int305/BartAppQA_Int305App.swift
@@ -0,0 +1,17 @@
+//
+//  BartAppQA_Int305App.swift
+//  BartAppQA-Int305
+//
+//  Created by Nate on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BartAppQA_Int305App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
